1886e2bce516c23d21dc69313cfb67c5a764e12f
snmimproving <snmxist21@gmail.com>
2026-09-22 00:26:35 -0600

    config base para subir a github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: ".",
   publicDir: "public",
+
+  base: "/flores-amarillas/",
+
   build: {
     outDir: "dist",
     target: "es2020",
